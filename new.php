c03292f03f474efaead87eafa60081c76abd8710
Jagadeesh Gajula <jgajula@visa.com>
2015-03-08 19:55:57 -0700

    updating files

--- new.php
+++ new.php
@@ -117,68 +117,43 @@
 
 <div id="contact-page" class="container">
 
-    <div class="bg">
-        <div class="row">
-            <div class="col-sm-8">
-                <div class="contact-form">
-                    <h2 class="title text-center">Get In Touch</h2>
-                    <div class="status alert alert-success" style="display: none"></div>
-                    <form id="main-contact-form" class="contact-form row" name="contact-form" method="post">
-                        <div class="form-group col-md-6">
-                            <input type="text" name="name" class="form-control" required="required" placeholder="Name">
-                        </div>
-                        <div class="form-group col-md-6">
-                            <input type="email" name="email" class="form-control" required="required" placeholder="Email">
-                        </div>
-                        <div class="form-group col-md-12">
-                            <input type="text" name="subject" class="form-control" required="required" placeholder="Subject">
-                        </div>
-                        <div class="form-group col-md-12">
-                            <textarea name="message" id="message" required="required" class="form-control" rows="8" placeholder="Your Message Here"></textarea>
-                        </div>
-                        <div class="form-group col-md-12">
-                            <input type="submit" name="submit" class="btn btn-primary pull-right" value="Submit">
-                        </div>
-                    </form>
-                </div>
-            </div>
-            <div class="col-sm-4">
-                <div class="contact-info">
-                    <h2 class="title text-center">Contact Info</h2>
-                    <address>
-                        <p>Mystyle Inc.</p>
-                        <p>355 West San Fernando Street, San Jose, CA 95113</p>
-                        <p>Mobile: +1 888 488 4968</p>
-                        <p>Fax: +1 888 488 4968</p>
-                        <p>Email: info@mystyle.com</p>
-                    </address>
-                    <div class="social-networks">
-                        <h2 class="title text-center">Social Networking</h2>
-                        <ul>
-                            <li>
-                                <a href="https://www.facebook.com/pages/Unofficial-MyStyle/456566094522842"><i class="fa fa-facebook"></i></a>
-                            </li>
-                            <li>
-                                <a href="https://twitter.com/iwmy2012"><i class="fa fa-twitter"></i></a>
-                            </li>
-                            <li>
-                                <a href="https://plus.google.com/100066114457703852702"><i class="fa fa-google-plus"></i></a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div id="mapwrapper">
-    <iframe
-            width="600"
-            height="450"
-            frameborder="0" style="border:0"
-            src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBTMkZQI07DSZ0sy0wB4WH5vKbk9hmV2j4
-    &q=ITU, West San Fernando Street, San Jose, CA">
-    </iframe>
-    </div>
+<div class="col-sm-9 padding-right col-sm-offset-1">
+					<div class="features_items"><!--features_items-->
+						<h2 class="title text-center">New Items</h2>
+<?php
+require_once("dbcontroller.php");
+$db_handle = new DBController();
+	$product_array = $db_handle->runQuery("SELECT * FROM products ORDER BY id ASC");
+	if (!empty($product_array)) {
+		foreach($product_array as $key=>$value){
+	?>
+						<div class="col-sm-4">
+							<div class="product-image-wrapper">
+								<div class="single-products">
+									<div class="productinfo text-center">
+										<img src="images/<?php echo $product_array[$key]["image"]; ?>">
+										<h2>$<?php echo $product_array[$key]["price"]; ?></h2>
+										<p><?php echo $product_array[$key]["name"]; ?></p>
+										<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
+									</div>
+									<div class="product-overlay">
+										<div class="overlay-content">
+											<h2>$<?php echo $product_array[$key]["price"]; ?></h2>
+											<p><?php echo $product_array[$key]["name"]; ?></p>
+											<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+	<?php
+			}
+	}
+	?>
+
+
+					</div><!--features_items-->
+				</div>
 
 </div>
 
